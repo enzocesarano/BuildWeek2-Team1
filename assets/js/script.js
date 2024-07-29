84ecedcdbb22a0bda0819d4cf0fa4af205a1d874
Vincenzo Cesarano <vincenzo.cesarano92@gmail.com>
2024-07-29 16:41:53 +0200

    BuildWeek

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+const dropdown = document.getElementById('dropdown')
+const destra = document.getElementById('destra')
+const center = document.getElementById('center')
+
+dropdown.addEventListener('click', function(){
+    destra.classList.toggle('display')
+})
